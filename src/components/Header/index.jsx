4a44bdc93933074c1d24b01900e07e67e112910a
Miracle-Mun <wg0319736@gmail.com>
2023-01-26 15:27:45 +0900

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import SideBar from '../../components/SideBar';
+import { useResize } from "../../utils/Helper";
 import './header.scss';
 
 const Header = () => {
   const { pathname } = useLocation()
-
+  const { isMobile } = useResize()
   return (
     <div className='headerwrap'>
       <div className='headercontainer'>
-        <a href='/'><div className='logo'>
-          <img src={pathname === '/client' ? '/assets/images/logo1.png' : '/assets/images/logo2.png'} className='logos' alt='' />
-        </div></a>
-        <div className='btns'>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Sobre a BeGlam</div>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Como usar</div>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Nossos Serviços</div>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Avaliações</div>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Por Aí</div>
-          <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Contato</div>
+        <div className='logo'>
+          <Link to='/'><img src={pathname === '/client' ? '/assets/images/logo1.png' : '/assets/images/logo2.png'} className='logos' alt='' /></Link>
         </div>
+        {!isMobile &&
+          <div className='btns'>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Sobre a BeGlam</div>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Como usar</div>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Nossos Serviços</div>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Avaliações</div>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Por Aí</div>
+            <div className={pathname === '/client' ? 'btn1' : 'btn2'}>Contato</div>
+          </div>
+        }
       </div>
-      {/* <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} /> */}
+      {isMobile && <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />}
     </div>
   )
 }
